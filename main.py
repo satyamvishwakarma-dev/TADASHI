5f051b6d704308d3acfd1ce6385cc237d22c9769
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-02-20 20:44:03 +0530

    testing the API

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from google import genai
+clint = genai.Client(api_key="AIzaSyCnpezmQduXKB0LsUHtrDDkZyROq4LnpS0")
+
+prompt = input("Enter the prompt: ")
+resource = clint.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+
+
+print("TADASHI -> " + resource.text)
